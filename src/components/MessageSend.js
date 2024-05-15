376cd5203cff8b84aeef17f87a86bc18a6051b42
yubahwesc@gmail.com <yubahwesc@gmail.com>
2024-05-15 16:34:42 +0200

    components

--- src/components/MessageSend.js
+++ src/components/MessageSend.js
@@ -1,32 +1,42 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { allMessagesBetweenUsers } from '../actions/socketAction';
 
 const MessageSend = ({ userId, contactId }) => {
-  const dispatch = useDispatch()
-  const chats = useSelector((state) => state.getMessages)
-  const { messages } = chats
+  const dispatch = useDispatch();
+  const chats = useSelector((state) => state.getMessages);
+  const { messages } = chats;
   const token = localStorage.getItem('token');
-  const payload = JSON.parse(atob(token.split('.')[1]));
 
   useEffect(() => {
     dispatch(allMessagesBetweenUsers(contactId, token));
   }, [contactId]);
 
+  const formatMessageTime = (timeString) => {
+    const time = new Date(timeString);
+    const options = { hour: '2-digit', minute: '2-digit', hour12: true };
+    return time.toLocaleTimeString([], options);
+  };
+
   return (
     <>
-      {!messages ?
-        <h1>there is not yet message</h1> :
-        messages && messages.map((mess, index) => (
+      {!messages ? (
+        <h1>There are no messages yet</h1>
+      ) : (
+        messages.length > 0 &&
+        messages.map((mess, index) => (
           <div key={index} className='messageSend owner'>
-            <div className='messageContent' style={{ alignItems: mess.senderId === userId ? 'flex-end' : "flex-start" }}>
-              <img className='imgInfo' src="https://media.istockphoto.com/id/1322277517/photo/wild-grass-in-the-mountains-at-sunset.jpg?s=612x612&w=0&k=20&c=6mItwwFFGqKNKEAzv0mv6TaxhLN3zSE43bWmFN--J5w=" />
-              <p className='para text-light fs-6' style={{ backgroundColor: mess.senderId === userId ? "#8da4f1" : "#3333ff" }}>{mess.message}</p>
+            <div className='messageContent' style={{ alignItems: mess.senderId === userId ? 'flex-end' : 'flex-start' }}>
+              <p className='para text-light fs-6' style={{ backgroundColor: mess.senderId === userId ? '#8da4f1' : '#3333ff' }}>
+                {mess.message}
+                <span className="message-time">{formatMessageTime(mess.createdOn)}</span>
+              </p>
             </div>
           </div>
-        ))}
+        ))
+      )}
     </>
-  )
-}
+  );
+};
 
-export default MessageSend
+export default MessageSend;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,12 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const SearchBar = () => {
+const SearchBar = ({ handleSearch }) => {
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const handleChange = (e) => {
+    setSearchQuery(e.target.value);
+    handleSearch(e.target.value);
+  };
   return (
-    
     <div className='search'>
-        <div className='searchForm'>
-            <input type='text' placeholder='search here.....'/>
-        </div>
+      <div className='searchForm'>
+        <input
+          type='text'
+          placeholder='search here.....'
+          style={{ "padding": "8px" }}
+          value={searchQuery}
+          onChange={handleChange}
+        />
+      </div>
     </div>
   )
 }
