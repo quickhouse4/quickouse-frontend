6b36835ac5c1b5d628fc90ef97566f8450ab0b40
yubahwesc@gmail.com <yubahwesc@gmail.com>
2024-06-08 11:08:32 +0200

    my list

--- src/screen/Transactions.js
+++ src/screen/Transactions.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react'
-import Header from '../components/Header'
+import React, { useEffect, useState } from 'react';
+import Header from '../components/Header';
 import Sidebar from './Sidebar';
 import { useDispatch, useSelector } from 'react-redux';
 import DataTable from '../components/DataTable';
@@ -11,19 +11,19 @@ import moment from 'moment';
 
 const Transactions = () => {
     const [label, setLabel] = useState('');
-    const token = localStorage.getItem("token")
-    const { transactions, loading } = useSelector((state) => state.getTransactions)
-    const dispatch = useDispatch()
+    const token = localStorage.getItem("token");
+    const { transactions, loading } = useSelector((state) => state.getTransactions);
+    const dispatch = useDispatch();
 
     useEffect(() => {
-        dispatch(getTransactionAction(token))
-    }, [])
+        dispatch(getTransactionAction(token));
+    }, [dispatch, token]);
 
     const columns = [
         {
             Header: "Date",
             accessor: "updatedOn",
-            Cell: ({ value }) => moment(value).format("MMM Do YY")
+            Cell: ({ value }) => moment(value).format("MMM Do YY"),
         },
         {
             Header: "Client",
@@ -45,65 +45,48 @@ const Transactions = () => {
             Header: "Amount",
             accessor: "amount",
         },
-        // {
-        //     Header: "Action",
-        //     accessor: "",
-        //     Cell: ({ row }) => (
-        //         <h1></h1>
-        //     ),
-        // },
     ];
 
     return (
         <>
             <Header setLabel={setLabel} />
-            <div class="w-100 pt-2">
-                <div class="d-flex position-relative flex-nowrap">
+            <div className="w-100 pt-2">
+                <div className="d-flex position-relative flex-nowrap">
                     <Sidebar />
-                    {
-                        transactions.length === 0 ?
-                            <h2 class="d-flex justify-content-center align-items-center flex-grow-1">
-                                {" "}
-                                <span className='fs-3'>No Transactions</span>
-                            </h2>
-                            : loading ?
-                                <h2 class="d-flex justify-content-center align-items-center flex-grow-1">
-                                    {" "}
-                                    <Spinner
-                                        animation="border"
-                                        variant="dark"
-                                    />
-                                </h2>
-                                :
-                                (
-                                    <div className='mb-4' style={{ marginTop: "117px" }}>
-                                        {
-                                            transactions.length > 0 && (
-                                                <DataTable
-                                                    data={transactions}
-                                                    columns={columns}
-                                                    title="Transactions"
-                                                    placeholder=""
-                                                />
-                                            )}
-
-                                        <div class="flex-nowrap mt-5 w-full justify-content-end mr-5">
-                                            <div className='d-flex justify-content-end align-items-center gap-3'>
-                                                <h3 className='fs-5'>CASHIN</h3>
-                                                <span className=''>RWF {calculateTotalCashIn(transactions)}</span>
-                                            </div>
-                                            <div className='d-flex justify-content-end align-items-center gap-3'>
-                                                <h3 className='fs-5'>CASHOUT</h3>
-                                                <span className=''>RWF {calculateTotalCashOut(transactions)}</span>
-                                            </div>
-                                        </div>
-                                    </div>
-                                )}
+                    {loading ? (
+                        <div className="d-flex justify-content-center align-items-center flex-grow-1">
+                            <Spinner animation="border" variant="dark" />
+                        </div>
+                    ) : transactions.length === 0 ? (
+                        <div className="d-flex justify-content-center align-items-center flex-grow-1">
+                            <h2 className='fs-3'>No Transactions</h2>
+                        </div>
+                    ) : (
+                        <div className='mb-4' style={{ marginTop: "117px" }}>
+                            {transactions.length > 0 && (
+                                <DataTable
+                                    data={transactions}
+                                    columns={columns}
+                                    title="Transactions"
+                                />
+                            )}
+                            <div className="flex-nowrap mt-5 w-full justify-content-end mr-5">
+                                <div className='d-flex justify-content-end align-items-center gap-3'>
+                                    <h3 className='fs-5'>CASHIN</h3>
+                                    <span>RWF {calculateTotalCashIn(transactions)}</span>
+                                </div>
+                                <div className='d-flex justify-content-end align-items-center gap-3'>
+                                    <h3 className='fs-5'>CASHOUT</h3>
+                                    <span>RWF {calculateTotalCashOut(transactions)}</span>
+                                </div>
+                            </div>
+                        </div>
+                    )}
                 </div>
             </div>
             <Footer />
         </>
-    )
-}
+    );
+};
 
-export default Transactions
+export default Transactions;
